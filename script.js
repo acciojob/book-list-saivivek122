2dae5822fa9375facb813d5747590db726b15586
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-14 12:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+let title=document.getElementById("title");
+let author=document.getElementById("author");
+let isbn=document.getElementById("isbn");
+let button=document.getElementById("submit")
+let table=document.getElementById("book-list")
+
+
+function getBookDetails(){
+	if(title.value=="" ||author.value=="" ||isbn.value==""){
+		return;
+	}
+	let tr=table.insertRow();
+	let deleteButton=document.createElement("button");
+	deleteButton.id="delete"
+	deleteButton.textContent="delete";
+	let td1=tr.insertCell();
+	td1.className="tabledata"
+	let td2=tr.insertCell();
+	td2.className="tabledata"
+	let td3=tr.insertCell();
+	td3.className="tabledata";
+	tr.appendChild(deleteButton);
+	td1.textContent=title.value;
+	td2.textContent=author.value;
+	td3.textContent=isbn.value;
+	title.value="";
+	author.value="";
+	isbn.value=""
+
+	deleteButton.addEventListener("click",()=>{
+		tr.remove();
+	});
+
+}
+
+
+
+
+button.addEventListener("click",getBookDetails)
